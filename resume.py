d083feb9085966014e58f05149ed23579e786285
Samir Sengupta <51231316+SamirSengupta@users.noreply.github.com>
2023-05-04 10:33:49 +0530

    Add files via upload

--- resume.py
+++ resume.py
@@ -1,13 +1,8 @@
-from PIL import Image, ImageDraw, ImageOps
 import streamlit as st
-import requests
-from io import BytesIO
+from PIL import Image, ImageDraw, ImageOps
 
 # get profile picture
-profile_pic_url = 'https://media.licdn.com/dms/image/D4D03AQGTuF62saxbiw/profile-displayphoto-shrink_800_800/0/1669709675567?e=1682553600&v=beta&t=OUnLlbCIQWYNygSHkV6ZfsBKmfOu6R8UOHcv3Tbtadg'
-response = requests.get(profile_pic_url)
-profile_pic = Image.open(BytesIO(response.content))
-
+profile_pic = Image.open("Profile.jpg")
 # resize profile picture
 size = (300, 300)
 profile_pic = profile_pic.resize(size)
@@ -27,7 +22,6 @@ st.image(output, caption='', width=150, use_column_width=False)
 
 st.write('# Samir Sengupta')
 
-
 # Add contact information
 st.write('**Contact Information:**')
 st.write('- Mumbai (Mira Road)')
@@ -35,62 +29,49 @@ st.write('- +91-8356075699')
 st.write('- Samir843301003@gmail.com')
 
 
-# Add about section
-st.write('## About')
-st.write('I am pursuing my bachelor\'s degree in the field of Data Science currently in the final semester. I have experience of real-time projects and have completed a few projects dealing with the EDA and Data Cleaning process and built a few regressions as well as classification-based projects during my academics. I have expertise in data visualization and representation which can provide good insights and sound information about the data.')
-
 # Add education section
 st.write('## Education')
-col1, col2 = st.columns([1, 4])
-with col1:
-    st.write('**Bachelor of Science in Data Science,**')
-    st.write('KES Shroff College, Mumbai')
-    st.write('3 Years (2020-2023)')
-with col2:
-    st.write('**Higher Secondary School,**')
-    st.write('Reena Mehta Jr College, Mumbai')
-    st.write('2 Years (2018-2020)')
-    st.write('**Secondary School,**')
-    st.write('St. Thomas High School, Mumbai')
-    st.write('Completed in 2018')
+education_list = [    {'degree': 'BACHELOR OF DATA SCIENCE', 'institution': 'KES Shroff College, Mumbai', 'duration': '3 Years (2020-2023)'},    {'degree': 'HIGHER SECONDARY SCHOOL', 'institution': 'Reena Mehta Jr College, Mumbai', 'duration': '2 Years (2018-2020)'},    {'degree': 'SECONDARY SCHOOL', 'institution': 'St. Thomas High School, Mumbai', 'duration': 'Completed in 2018'}]
+
+# Create a table with the education information
+table = "| Degree | Institution | Duration |\n| ------ | ----------- | -------- |\n"
+for education in education_list:
+    table += f"| {education['degree']} | {education['institution']} | {education['duration']} |\n"
+
+st.write(table, markdown=True)
+
+
 
 # Add projects section
 st.write('## Projects')
-st.write('- Built a chatbot with GUI')
-st.write('- Twitter Sentimental Analysis')
 st.write('- Movie Recommendation System')
-st.write('- Alexa Bot')
+st.write('- Twitter Sentimental Analysis')
 
 # Add certifications section
 st.write('## Certifications')
 st.write('- Advanced Excel')
 st.write('- Arduino Programming')
 st.write('- Blockchain Technology')
-st.write('- Recommender System')
+st.write('- Machine Learning')
+st.write('- PowerBi')
 
 # Add skills section
 st.write('## Skills & Abilities')
-st.write('### Technical Skills')
-st.write('- Programming Languages & Essentials: C++, Python, R Programming, MySQL, Tableau, Power Bi, HTML, CSS And JavaScript, Adv Excel.')
-st.write('### Areas of Interest')
-st.write('- SQL')
-st.write('- Machine Learning')
-st.write('- Natural Language Processing')
-st.write('- Deep Learning')
-st.write('- Software Engineering')
-st.write('- Networking')
-st.write('- Cloud Computing')
+st.write('- Back-End Development')
 st.write('- Data Analytics')
+st.write('- Data Visualization')
+st.write('- Front-End Development')
+st.write('- Machine Learning')
+st.write('- Networking')
+st.write('- Python')
+st.write('- Statistics')
 
 # Add personal abilities section
 st.write('## Personal Abilities')
 st.write('- Ability to make sound decisions')
 st.write('- Good communication and organizing skills')
 st.write('- Ability to make people understand the situation quickly')
-st.write('- Pragmatic and effective risk management.')
 st.write('- Quick decision maker.')
-st.write('- Proficient in both spoken and written English.')
-st.write('- Skilled in creative writing.')
 
 # Add language section
 st.write('## Languages')
@@ -127,4 +108,4 @@ with col3:
 
 # Add footer
 st.write('---')
-st.write('© Samir Sengupta  |  Last updated: February 2023')
+st.write('© Samir Sengupta  |  Last updated: May 2023')
